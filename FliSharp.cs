3abfc65a07afa3ad48e25af8ad010d5b1556d623
eliotg <eliotgillum@hotmail.com>
2015-10-20 17:56:09 -0700

    finishing enum section

--- FliSharp.cs
+++ FliSharp.cs
@@ -11,14 +11,14 @@ namespace FliSharp
 
         /// <summary>
         /// The domain of an FLI device.  This consists of a bitwise ORed
-        ///    combination of interface method and device type.  Valid interfaces
-        ///    are \texttt{FLIDOMAIN_PARALLEL_PORT}, \texttt{FLIDOMAIN_USB},
-        ///    \texttt{FLIDOMAIN_SERIAL}, and \texttt{FLIDOMAIN_INET}.  Valid
-        ///    device types are \texttt{FLIDEVICE_CAMERA},
-        ///    \texttt{FLIDOMAIN_FILTERWHEEL}, and \texttt{FLIDOMAIN_FOCUSER}.
+        /// combination of interface method and device type.  Valid interfaces
+        /// are \texttt{FLIDOMAIN_PARALLEL_PORT}, \texttt{FLIDOMAIN_USB},
+        /// \texttt{FLIDOMAIN_SERIAL}, and \texttt{FLIDOMAIN_INET}.  Valid
+        /// device types are \texttt{FLIDEVICE_CAMERA},
+        /// \texttt{FLIDOMAIN_FILTERWHEEL}, and \texttt{FLIDOMAIN_FOCUSER}.
         ///    
-        ///    @see FLIOpen
-        ///    @see FLIList
+        /// @see FLIOpen
+        /// @see FLIList
         /// </summary>
         [Flags]
         public enum DOMAIN : long
@@ -41,9 +41,9 @@ namespace FliSharp
 
         /// <summary>
         /// The frame type for an FLI CCD camera device.  Valid frame types are
-        ///    \texttt{FLI_FRAME_TYPE_NORMAL} and \texttt{FLI_FRAME_TYPE_DARK}.
+        /// \texttt{FLI_FRAME_TYPE_NORMAL} and \texttt{FLI_FRAME_TYPE_DARK}.
         ///
-        ///    @see FLISetFrameType
+        /// @see FLISetFrameType
         /// </summary>
         [Flags]
         public enum FRAME_TYPE : long
@@ -56,9 +56,9 @@ namespace FliSharp
 
         /// <summary>
         /// The gray-scale bit depth for an FLI camera device.  Valid bit
-        ///    depths are \texttt{FLI_MODE_8BIT} and \texttt{FLI_MODE_16BIT}.
+        /// depths are \texttt{FLI_MODE_8BIT} and \texttt{FLI_MODE_16BIT}.
         ///
-        ///    @see FLISetBitDepth
+        /// @see FLISetBitDepth
         /// </summary>
         public enum BIT_DEPTH : long
         {
@@ -68,13 +68,13 @@ namespace FliSharp
 
         /// <summary>
         /// Type used for shutter operations for an FLI camera device.  Valid
-        ///    shutter types are \texttt{FLI_SHUTTER_CLOSE},
-        ///    \texttt{FLI_SHUTTER_OPEN},
-        ///    \texttt{FLI_SHUTTER_EXTERNAL_TRIGGER},
-        ///    \texttt{FLI_SHUTTER_EXTERNAL_TRIGGER_LOW}, and
-        ///    \texttt{FLI_SHUTTER_EXTERNAL_TRIGGER_HIGH}.
+        /// shutter types are \texttt{FLI_SHUTTER_CLOSE},
+        /// \texttt{FLI_SHUTTER_OPEN},
+        /// \texttt{FLI_SHUTTER_EXTERNAL_TRIGGER},
+        /// \texttt{FLI_SHUTTER_EXTERNAL_TRIGGER_LOW}, and
+        /// \texttt{FLI_SHUTTER_EXTERNAL_TRIGGER_HIGH}.
         ///
-        ///    @see FLIControlShutter
+        /// @see FLIControlShutter
         /// </summary>
         public enum SHUTTER : long
         {
@@ -88,10 +88,10 @@ namespace FliSharp
 
         /// <summary>
         /// Type used for background flush operations for an FLI camera device.  Valid
-        ///    bgflush types are \texttt{FLI_BGFLUSH_STOP} and
-        ///    \texttt{FLI_BGFLUSH_START}.
+        /// bgflush types are \texttt{FLI_BGFLUSH_STOP} and
+        /// \texttt{FLI_BGFLUSH_START}.
         ///
-        ///    @see FLIControlBackgroundFlush
+        /// @see FLIControlBackgroundFlush
         /// </summary>
         public enum BGFLUSH : long
         {
@@ -101,10 +101,10 @@ namespace FliSharp
 
         /// <summary>
         /// Type used to determine which temperature channel to read.  Valid
-        ///    channel types are \texttt{FLI_TEMPERATURE_INTERNAL} and
-        ///    \texttt{FLI_TEMPERATURE_EXTERNAL}.
+        /// channel types are \texttt{FLI_TEMPERATURE_INTERNAL} and
+        /// \texttt{FLI_TEMPERATURE_EXTERNAL}.
         ///
-        ///    @see FLIReadTemperature
+        /// @see FLIReadTemperature
         /// </summary>
         public enum CHANNEL : long
         {
@@ -112,6 +112,69 @@ namespace FliSharp
             EXTERNAL = 0x0001,
             CCD = 0x0000,
             BASE = 0x0001
+        }
+
+        /// <summary>
+        /// Type specifying library debug levels.  Valid debug levels are
+        /// \texttt{FLIDEBUG_NONE}, \texttt{FLIDEBUG_INFO},
+        /// \texttt{FLIDEBUG_WARN}, and \texttt{FLIDEBUG_FAIL}.
+        ///
+        /// @see FLISetDebugLevel
+        /// </summary>
+        [Flags]
+        public enum DEBUG : long
+        {
+            NONE = 0x00,
+            INFO = 0x01,
+            WARN = 0x02,
+            FAIL = 0x04,
+            IO = 0x08,
+            ALL = INFO | WARN | FAIL
+        }
+
+        [Flags]
+        public enum FAN_SPEED : long
+        {
+            OFF = 0x00,
+            ON = 0xffffffff
+        }
+
+        /// <summary>
+        /// Status settings
+        /// </summary>
+        [Flags]
+        public enum STATUS : long
+        {
+            CAMERA_STATUS_UNKNOWN = 0xffffffff,
+            CAMERA_STATUS_MASK = 0x00000003,
+            CAMERA_STATUS_IDLE = 0x00,
+            CAMERA_STATUS_WAITING_FOR_TRIGGER = 0x01,
+            CAMERA_STATUS_EXPOSING = 0x02,
+            CAMERA_STATUS_READING_CCD = 0x03,
+            CAMERA_DATA_READY = 0x80000000,
+
+            FOCUSER_STATUS_UNKNOWN = 0xffffffff,
+            FOCUSER_STATUS_HOMING = 0x00000004,
+            FOCUSER_STATUS_MOVING_IN = 0x00000001,
+            FOCUSER_STATUS_MOVING_OUT = 0x00000002,
+            FOCUSER_STATUS_MOVING_MASK = 0x00000007,
+            FOCUSER_STATUS_HOME = 0x00000080,
+            FOCUSER_STATUS_LIMIT = 0x00000040,
+            FOCUSER_STATUS_LEGACY = 0x10000000,
+
+            FILTER_WHEEL_PHYSICAL = 0x100,
+            FILTER_WHEEL_VIRTUAL = 0,
+            FILTER_WHEEL_LEFT = FILTER_WHEEL_PHYSICAL | 0x00,
+            FILTER_WHEEL_RIGHT = FILTER_WHEEL_PHYSICAL | 0x01,
+            FILTER_STATUS_MOVING_CCW = 0x01,
+            FILTER_STATUS_MOVING_CW = 0x02,
+            FILTER_POSITION_UNKNOWN = 0xff,
+            FILTER_POSITION_CURRENT = 0x200,
+            FILTER_STATUS_HOMING = 0x00000004,
+            FILTER_STATUS_HOME = 0x00000080,
+            FILTER_STATUS_HOME_LEFT = 0x00000080,
+            FILTER_STATUS_HOME_RIGHT = 0x00000040,
+            FILTER_STATUS_HOME_SUCCEEDED = 0x00000008
         }
 
         #endregion
